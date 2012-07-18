ff512e2bb25010968d220af027c37af7b2dc3090
cgeek <cgeek.share@gmail.com>
2012-07-18 16:13:38 +0800

    index

--- WebRoot/protected/views/layouts/waterfall.php
+++ WebRoot/protected/views/layouts/waterfall.php
@@ -2,15 +2,15 @@
 	<?php if(!empty($pin_list)):?>
 		<?php foreach($pin_list as $pin):?>
 	<div class="pin item">
-		<div class="cover_image"><a href="/pin/<?=$pin['pin_id'];?>"><img width=222 height="<?=$pin['cover_image_height'];?>" src="<?=$pin['cover_image_mw'];?>" ></a></div>
-		<p class="title"><a href="/pin/<?=$pin['pin_id'];?>"><?=$pin['title'];?></a></p>
+		<div class="cover_image"><a target="_blank" href="/pin/<?=$pin['pin_id'];?>"><img width=222 height="<?=$pin['cover_image_height'];?>" src="<?=$pin['cover_image_mw'];?>" ></a></div>
+		<p class="title"><a target="_blank" href="/pin/<?=$pin['pin_id'];?>"><?=$pin['title'];?></a></p>
 		<p class="content">	<?=$pin['desc'];?> </p>
 		<div class="author">
-			<a class="author_avatar" href="/user/<?=$pin['user']['user_id'];?>">
+			<a target="_blank" class="author_avatar" href="/user/<?=$pin['user']['user_id'];?>">
 				<img src="<?=$pin['user']['avatar'];?>" width=30 > 
 			</a>
 			<div class="username">
-				<a href="/user/<?=$pin['user']['user_id'];?>" target="_blank"><?=$pin['user']['user_name'];?> </a>
+				<a target="_blank" href="/user/<?=$pin['user']['user_id'];?>" target="_blank"><?=$pin['user']['user_name'];?> </a>
 			<br>
 			<span><?=$pin['ctime'];?></span>
 			</div>
@@ -27,15 +27,15 @@
 <script type="js/template" id="item_list_tpl">
 {{#pin_list}}
 <div class="pin item">
-	<div class="cover_image"><a href="/pin/{{pin_id}}"><img width=222 height="{{cover_image_height}}" src="{{cover_image_mw}}" ></a></div>
-	<p class="title"><a href="/pin/{{pin_id}}">{{title}}</a></p>
+	<div class="cover_image"><a target="_blank" href="/pin/{{pin_id}}"><img width=222 height="{{cover_image_height}}" src="{{cover_image_mw}}" ></a></div>
+	<p class="title"><a target="_blank" href="/pin/{{pin_id}}">{{title}}</a></p>
 	<p class="content">{{{desc}}}</p>
 	<div class="author">
-	<a class="author_avatar" href="/user/{{user.user_id}}">
+	<a target="_blank" class="author_avatar" href="/user/{{user.user_id}}">
 	<img src="{{user.avatar}}" width=30 > 
 	</a>
 	<div class="username">
-	<a href="/user/{{user.user_id}}" target="_blank">{{user.user_name}} </a>
+	<a target="_blank" href="/user/{{user.user_id}}" target="_blank">{{user.user_name}} </a>
 	<br>
 	<span>{{ctime}}</span>
 	</div>
--- WebRoot/protected/views/pin/index.php
+++ WebRoot/protected/views/pin/index.php
@@ -5,13 +5,13 @@
 				<div class="item active">
 					<img src="http://qimages.b0.upaiyun.com/bcdd3786761b75cecb5dcf25d0e2ccb4_big.jpg" height="250" width="555">
 					<div class="carousel-caption">
-						<h4><a href="/pin/17" style="color:#fff;">捷星航空新加坡～大阪特价，特惠票价新加坡元168起</a></h4>
+						<h4><a target="_blank" href="/pin/17" style="color:#fff;">捷星航空新加坡～大阪特价，特惠票价新加坡元168起</a></h4>
 					</div>
 				</div>
 				<div class="item">
 					<img src="http://qimages.b0.upaiyun.com/6e1a962bfdaece8336435010b527eb74_big.jpg" height="250" width=555>
 					<div class="carousel-caption">
-						<h4><a href="/pin/13" style="color:#fff;">亚洲航空机票预订超级详细图解</h4>
+						<h4><a target="_blank" href="/pin/13" style="color:#fff;">亚洲航空机票预订超级详细图解</a></h4>
 					</div>
 				</div>
 			</div>
@@ -67,7 +67,7 @@ skyscanner.setOnLoadCallback(main);
 		</div>
 <?php if(!Yii::app()->user->isGuest):?>
 		<div class="add_pin">
-			<a href="/pin/add"> + 发布特价信息</a>
+			<a target="_blank" href="/pin/add"> + 发布特价信息</a>
 		</div>
 <?php endif;?>
 	</div>
