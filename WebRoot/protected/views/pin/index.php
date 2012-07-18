25066ec1e13084d62db9e041675f599fc4d8ae11
cgeek <cgeek.share@gmail.com>
2012-07-18 16:17:12 +0800

    index

--- WebRoot/protected/views/pin/index.php
+++ WebRoot/protected/views/pin/index.php
@@ -3,13 +3,13 @@
 		<div id="pinCarousel" class="carousel slide" style="width:555px;float:left;height:250px;">
 			<div class="carousel-inner">
 				<div class="item active">
-					<img src="http://qimages.b0.upaiyun.com/bcdd3786761b75cecb5dcf25d0e2ccb4_big.jpg" height="250" width="555">
+					<a target="_blank" href="/pin/17"> <img src="http://qimages.b0.upaiyun.com/bcdd3786761b75cecb5dcf25d0e2ccb4_big.jpg" height="250" width="555"> </a>
 					<div class="carousel-caption">
 						<h4><a target="_blank" href="/pin/17" style="color:#fff;">捷星航空新加坡～大阪特价，特惠票价新加坡元168起</a></h4>
 					</div>
 				</div>
 				<div class="item">
-					<img src="http://qimages.b0.upaiyun.com/6e1a962bfdaece8336435010b527eb74_big.jpg" height="250" width=555>
+					<a target="_blank" href="/pin/13"> <img src="http://qimages.b0.upaiyun.com/6e1a962bfdaece8336435010b527eb74_big.jpg" height="250" width=555></a>
 					<div class="carousel-caption">
 						<h4><a target="_blank" href="/pin/13" style="color:#fff;">亚洲航空机票预订超级详细图解</a></h4>
 					</div>
