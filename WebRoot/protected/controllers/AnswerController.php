378cfd10b0e06f9c3bb6cc7a6648eae1138eee27
cgeek <cgeek.share@gmail.com>
2013-01-15 14:22:00 +0800

    answer

--- WebRoot/protected/controllers/AnswerController.php
+++ WebRoot/protected/controllers/AnswerController.php
@@ -27,6 +27,7 @@ class AnswerController extends Controller
 					'user_avatar' => $user_db['avatar']
 				);
 			}
+			$answer['content'] = strip_tags($answer['content']);
 			$answer['ctime'] = human_time($answer['ctime']);
 			$answer['lat'] = empty($answer['lat']) ? '' : $answer['lat'];
 			$answer['lon'] = empty($answer['lon']) ? '' : $answer['lon'];
